d4b0acb84c5fe9cf2a771e6acf61377f18e9c1f8
Nick Collings <nick@collingo.com>
2014-07-22 16:26:19 +0100

    playing around

--- src/index.js
+++ src/index.js
@@ -1,26 +1,39 @@
 var $ = require('jquery');
-var Model = require('backbone').Model;
 var stitch = require('../../stitch/src/stitch');
 
 window.stitch = stitch;
 
 window.render = $('#render');
-window.unbound = $('#unbound');
+// window.unbound = $('#unbound');
 
-window.model = new Model({
-	text: "hello",
-	childCount: 9
-});
-window.person = new Model({
-	name: "Dave",
-	age: 25
-});
-window.personTpl = "<div><p>{{name}}</p><p>{{age}}</p></div>";
+// window.model = {
+// 	text: "hello",
+// 	childCount: 9
+// };
+// window.person = {
+// 	name: "Dave",
+// 	age: 25
+// };
+// window.personTpl = "<div><p>{{name}}</p><p>{{age}}</p></div>";
 
-$.ajax({
-	url: '/template.html',
-	async: false,
-	success: function(data) {
-		window.tpl = data;
-	}
-});
+// $.ajax({
+// 	url: '/template.html',
+// 	async: false,
+// 	success: function(data) {
+// 		window.tpl = data;
+// 	}
+// });
+
+window.itemTpl = '<li class="item" model="item" id="1">' +
+	'<div class="delete"></div>' +
+	'<div class="children" show="{{childCount}}">{{childCount}}</div>' +
+	'<div class="state" show="{{!childCount}}">' +
+		'<input type="checkbox" checked="{{done}}" />' +
+	'</div>' +
+	'<div class="text">{{text}}</div>' +
+'</li>';
+
+window.itemData = {
+	childCount: 3,
+	text: "Hello"
+};
